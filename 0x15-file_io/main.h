4883431fba0d9d245340841917cbd0a9d2318a35
Ayokunle Odutayo <kunlay.odutayo@gmail.com>
2023-12-01 15:25:56 -0800

    push task 2

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,9 +3,13 @@
 
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 
+int _putchar(char c)
+int _strlen(char *str)
 ssize_t read_textfile(const char *fileID, size_t alphabets);
 int create_file(const char *fileID, char *text_content);
 int append_text_to_file(const char *fileID, char *text_content);
